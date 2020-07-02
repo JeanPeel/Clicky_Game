bd8a544e536ac3bbaa89424959b582c06476cf71
Jean Peel <Jean.Peel.246@gmail.com>
2020-07-01 19:22:34 -0600

    fixed error

--- my-app/src/components/NavMessage/index.js
+++ my-app/src/components/NavMessage/index.js
@@ -4,48 +4,48 @@ import "./style.css";
 // NavMessage renders an li tag containing an message for the user
 
 class NavMessage extends Component {
-    state = {
-        message: "",
-        animating: false
-    };
+  state = {
+    message: "",
+    animating: false
+  };
 
-    componentDidUpdate({ score, topScore }, prevState) {
-        cost newState =  { animating: true };
+  componentDidUpdate({ score, topScore }, prevState) {
+    const newState = { animating: true };
 
-        if (score === 0 && topScore === 0) {
-            newState.message = "";
-        } else if (score === 0 && topScore > 0) {
-            newState.message = "incorrect";
-        } else {
-            newState.message = "correct";
-        }
-
-        if (score !== this.props.score || this.state.message !== newState.message) {
-            this.setState(newState);
-        }
+    if (score === 0 && topScore === 0) {
+      newState.message = "";
+    } else if (score === 0 && topScore > 0) {
+      newState.message = "incorrect";
+    } else {
+      newState.message = "correct";
     }
 
-    renderMessage = () => {
-        switch (this.state.message) {
-            case "correct":
-                return "You guessed correctly!";
-            case "incorrect":
-                return "You guessed incorrectly!";
-            default:
-                return "Click an image to begin!";
-        }
-    };
-
-    render () {
-        return (
-            <li
-                className={this.state.animating ? this.state.message : ""}
-                onAnimationEnd={() => this.setState({ animating: false })}
-            >
-                {this.renferMessage()}
-            </li>
-        );
+    if (score !== this.props.score || this.state.message !== newState.message) {
+      this.setState(newState);
     }
+  }
+
+  renderMessage = () => {
+    switch (this.state.message) {
+    case "correct":
+      return "You guessed correctly!";
+    case "incorrect":
+      return "You guessed incorrectly!";
+    default:
+      return "Click an image to begin!";
+    }
+  };
+
+  render() {
+    return (
+      <li
+        className={this.state.animating ? this.state.message : ""}
+        onAnimationEnd={() => this.setState({ animating: false })}
+      >
+        {this.renderMessage()}
+      </li>
+    );
+  }
 }
 
 export default NavMessage;
